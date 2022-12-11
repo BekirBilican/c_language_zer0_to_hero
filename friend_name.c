85cfd4fc8304cc9ee2d063827134e0652c5fe511
BekirBilican <119291015+BekirBilican@users.noreply.github.com>
2022-12-11 16:07:42 +0300

    3
    3

--- friend_name.c
+++ friend_name.c
@@ -1,5 +1,21 @@
-// write the name of the person you like
-// if the name is yours print(Hurra this is me)
-// This is my bff. 
-
-// Good Luck
+int main (){
+  char name[30];
+   char szKey[] = "bekir";
+  char szInput[80];
+printf("\nEnter a name of the person u like\n");
+                        
+    scanf("%s", name);
+ 
+if (strcmp(name, "bekir") == 0 ) {
+    printf("Hurra its me\n"); 
+}
+else if (strcmp(name, "Bekir") == 0 ) {
+    printf("Hurra its me\n"); 
+}
+else {
+    printf ("Okay its %s\n", name);
+}
+    
+      
+  system ("pause");
+    }
